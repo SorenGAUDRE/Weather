69ddb6ff12108a072d2dd2dd1fc0a15007138a38
soren.GAUDRE <soren>
2024-10-08 11:53:24 +0200

    v1 done

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const token = 'cf7c01dc60d6008455c0c8715a6337e049478ae675a65ab7287e7bf76b1dd5d7'
 const codePostalInput = document.getElementById("postal-code");
 const menuDeroulant = document.getElementById("menuDeroulant");
 const regexCodePostal = /^\d{5}$/;
+const validerBtn = document.getElementById("valider-btn");
+const tmin = document.getElementById("temp-min");
+const tmax = document.getElementById("temp-max");
+const rainProba = document.getElementById("rain-probability");
+const sunshine = document.getElementById("sunshine");
+const stats = document.getElementById("stats");
+const nvRech = document.getElementById("nv_rech");
+
 
 // Fonction pour récupérer les communes en fonction d'un code postal
 async function getCommunesByPostalCode(codePostal) {
-    let code_return = 0 ;
     const url = `https://geo.api.gouv.fr/communes?codePostal=${codePostal}`;
     let tab = []
     try {
@@ -23,6 +30,19 @@ async function getCommunesByPostalCode(codePostal) {
         
 }
 
+async function getCommunesByName(name) {
+    const url = `https://api.meteo-concept.com/api/location/cities?token=cf7c01dc60d6008455c0c8715a6337e049478ae675a65ab7287e7bf76b1dd5d7&search=${name}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.cities[0].insee)
+        return data.cities[0].insee; // Retourne le tableau des noms)
+        
+        }
+        catch{(error => console.error("Erreur lors de la récupération des communes:", error))};      
+        
+}
+
 async function getTemp(code) {
         
     const urlmeteo = `https://api.meteo-concept.com/api/forecast/daily/0?token=cf7c01dc60d6008455c0c8715a6337e049478ae675a65ab7287e7bf76b1dd5d7&insee=${code}`;
@@ -31,15 +51,18 @@ async function getTemp(code) {
         const data = await response.json();
         console.log('______________________________________')
             console.log("Temps:", data); // Affiche les communes dans la console
-            let tab = [ data.forecast.tmin , data.forecast.tmax ]
-
-            return tab
+            tmin.innerText = data.forecast.tmin + "°C";
+            tmax.innerText = data.forecast.tmax + "°C";
+            rainProba.innerText = data.forecast.probarain + "%" ;
+            sunshine.innerText = data.forecast.sun_hours + "h";
+    
         }
         
         catch{(error => console.error("Erreur lors de la récupération des temps:", error))}
 
     
 }
+
  
 // Tableau vide pour les valeurs (remplis plus tard avec des valeurs)
 let valeursCommune = []; // Exemples fictifs
@@ -88,12 +111,37 @@ codePostalInput.addEventListener("input", async function() {
         console.log(codes)
         valeursCommune = codes ;
         menuDeroulant.style.display = "block";
+        validerBtn.style.display="block";
         // Mettre à jour le menu déroulant avec les options (actuellement vide)
         mettreAJourMenu(valeursCommune);  // Utilise les données du tableau
     } else {
         menuDeroulant.style.display = "none";
+        validerBtn.style.display="none";
+        stats.style.display="none";
+        nvRech.style.display="none";
     }
 });
+
+validerBtn.addEventListener("click",async function() {
+    if(menuDeroulant.selectedIndex==0){
+        alert("Veuillez choisir une option");
+    }
+    else{
+        stats.style.display="block";
+        nvRech.style.display="block";
+        console.log(menuDeroulant.options[menuDeroulant.selectedIndex].value)
+        let insee =  await getCommunesByName(menuDeroulant.options[menuDeroulant.selectedIndex].value);
+        getTemp(insee)
+    }
+})
+
+nvRech.addEventListener("click", async function(){
+    menuDeroulant.style.display = "none";
+    stats.style.display="none";
+    nvRech.style.display="none";
+    validerBtn.style.display="none";
+    codePostalInput.value="";
+})
 
 
 
